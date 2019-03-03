16c2312c241318cfe5c18a5671870f756c6ec6a0
rogerbf <roger@bergfa.lk>
2019-03-03 13:58:19 +0100

    update testRegex

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
-const { NODE_ENV } = process.env
-
 module.exports = {
-  testRegex:
-    NODE_ENV === `development`
-      ? `(/tests/development/.*\\.spec.js)$`
-      : `/tests/enroll.spec.js`,
+  testRegex: `(/tests/.*\\.spec.js)$`,
 }
