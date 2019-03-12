971f16e333d6d74fbd39ed7aab500777c73f1e33
rogerbf <roger@bergfa.lk>
2019-03-12 20:47:53 +0100

    use create from call-tree

--- source/tree.js
+++ source/tree.js
@@ -1,69 +1,11 @@
-import { concat, omit, call, map } from "call-tree"
-import { difference } from "simple-difference"
-
-const toString = value => Object.prototype.toString.call(value)
-
-const OBJECT = toString({})
-
-const wrapListener = listener => (...args) => listener(...args)
+import { create } from "call-tree"
 
 export default () => {
-  let current = {}
-  let next = current
-
-  const snapshot = () => {
-    if (difference(next, current) === null) {
-      next = { ...current }
-    }
-  }
-
-  const subscribe = listener => {
-    const listenerType = toString(listener)
-
-    if (listenerType !== OBJECT) {
-      throw new Error(`Expected ${ OBJECT }, received ${ listenerType }.`)
-    }
-
-    let subscribed = true
-
-    snapshot()
-
-    const wrapped = map(listener, wrapListener)
-
-    next = concat(next, wrapped)
-
-    return () => {
-      if (!subscribed) {
-        return void 0
-      }
-
-      subscribed = false
-
-      snapshot()
-
-      next = omit(next, wrapped)
-
-      return listener
-    }
-  }
-
-  const broadcast = (...args) => {
-    current = next
-
-    call(current, ...args)
-  }
-
-  const prepare = fn => {
-    current = next
-
-    const tree = fn(current)
-
-    return call.bind(null, tree)
-  }
+  const tree = create()
 
   return {
-    subscribe,
-    broadcast,
-    prepare,
+    broadcast: (...args) => tree(...args),
+    subscribe: tree.attach,
+    prepare: tree.prepare,
   }
 }
